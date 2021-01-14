50f399e55c47c509c54362e075550c2b9cf07575
Shoozle <sean@seanmurtagh.uk>
2021-01-14 16:21:32 +0000

    Combined each game into a single array to map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Game from './components/Game';
 
-
-
 function App() {
   const gameData = require('./data.json');
   const gameKeys = Object.keys(gameData);
-  const noofyears = Object.keys(gameData).length;
+  // const noofyears = Object.keys(gameData).length;
+  // const yearLength = gameData[gameKeys[0]].length;
+  // console.log(gameData[gameKeys[0]].Object)
+  // console.log(yearLength);
   // console.log(noofyears);
 
-  const [games, setGames ] = useState([]);
-  // console.log(gameKeys[1]);
+  const [games, setGames] = useState([]);
+  // console.log(Object.keys(gameData));
 
   // for (let i=0; i<noofyears; i++) {
   //   let gamesInAYear = gameData[gameKeys[i].length];
@@ -23,32 +23,80 @@ function App() {
   //       />
   //       )
   //   }
-    // gameData[gameKeys[i]]
+  // gameData[gameKeys[i]]
 
-    console.log(gameData)
+  //console.log(gameData)
 
-    // const newGame = {
-    //   title : gameData[gameKeys[1]][1].title,
-    //   score : gameData[gameKeys[1]][1].score,
-    //   beaten : gameData[gameKeys[1]][1].beaten,
-    //   likes : gameData[gameKeys[1]][1].likes,
-    //   dislikes : gameData[gameKeys[1]][1].dislikes
-    // }
+  // const newGame = {
+  //   title : gameData[gameKeys[1]][1].title,
+  //   score : gameData[gameKeys[1]][1].score,
+  //   beaten : gameData[gameKeys[1]][1].beaten,
+  //   likes : gameData[gameKeys[1]][1].likes,
+  //   dislikes : gameData[gameKeys[1]][1].dislikes
+  // }
+  let gameList = gameData[2020].map((game, i) =>
+    <Game
+      key={i}
+      title={game.title}
+      score={game.score}
+      beaten={game.beaten}
+      likes={game.likes}
+      dislikes={game.dislikes}
+      url={game.url}
+    />
+  )
 
-    const gameList = gameData[2020].map(game =>
-      <Game
-        title={game.title}
-        score={game.score}
-        beaten={game.beaten}
-        likes={game.likes}
-        dislikes={game.dislikes}
-        url={game.url}
-      />
-    )
+  let yearlen = gameData[2020].length;
+  console.log(yearlen);
+  console.log(gameKeys.length);
+
+  //gameList = [];
+  let newList = [];
+
+
+  // for (let i=0;i<gameKeys.length;i++) {
+  //   console.log(i)
+  //   gameList = gameData[gameKeys[i]].map((game, i) => 
+  //     <Game
+  //       key={i}
+  //       title={game.title}
+  //       score={game.score}
+  //       beaten={game.beaten}
+  //       likes={game.likes}
+  //       dislikes={game.dislikes}
+  //       url={game.url}
+  //     />
+  //   )
+  // }
+
+  for (let i = 0; i < gameKeys.length; i++) {
+    for (let j = 0; j < gameData[gameKeys[i]].length; j++) {
+      console.log(gameData[gameKeys[i]][j])
+      newList.push(gameData[gameKeys[i]][j])
+    }
+  }
+
+  console.log(newList)
+
+  gameList = newList.map((game, i) =>
+    <Game
+      key={i}
+      title={game.title}
+      score={game.score}
+      beaten={game.beaten}
+      likes={game.likes}
+      dislikes={game.dislikes}
+      url={game.url}
+    />
+  )
+
+
+
+
 
   return (
     <div className="App">
-    {gameList}
+      {gameList}
     </div>
   );
 }
