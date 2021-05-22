126a63d651055a8b91db2121a8344be8dfa57645
Shoozle <sean@seanmurtagh.uk>
2021-05-22 18:03:51 +0100

    Really basic search function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import GameList from './components/GameList';
+import Game from './components/Game';
 import Searchbar from './components/Searchbar';
 
 function App() {
 
   let gameList = [];
-  let filterList = [];
+  // let filterList = [];
 
   const gamesDB = require('../src/data.json');
   const yearsDB = Object.keys(gamesDB);
 
-  const [searchFor, setSearchFor] = useState(false)
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchFor, setSearchFor] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterList, setFilterList] = useState([]);
+
+  let statelessList = [];
+  let searchedGameList = [];
 
 
   // let searchTerm = 'Sekiro'
@@ -33,35 +38,58 @@ function App() {
   // }
   // // console.log(Object.entries(gamesDB)[0])
 
-    for (let i = 0; i < yearsDB.length; i++) {
-      gameList.push(
-        <GameList
-          key={i}
-          year={yearsDB[i]}
-          games={gamesDB[yearsDB[i]]}
-        />
-      )
-    }
-
-
+  for (let i = 0; i < yearsDB.length; i++) {
+    gameList.push(
+      <GameList
+        key={i}
+        year={yearsDB[i]}
+        games={gamesDB[yearsDB[i]]}
+      />
+    )
+  }
 
   const searchTermHandler = (e) => {
-    setSearchFor(true)
-    setSearchTerm(e.target.value)
+    setSearchFor(true);
+    setSearchTerm(e.target.value);
+    setFilterList([]);
     if (e.target.value.trim().length === 0) {
       setSearchFor(false)
+    } else if ((e.target.value.trim().length > 3)) 
+    {
+      for (let i = 0; i < yearsDB.length; i++) {
+        gamesDB[yearsDB[i]].forEach(element => {
+          if (element.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            setFilterList(filterList => [...filterList, element])
+          }
+        })
+      };
     }
   }
 
+  console.log(filterList)
+  console.log(statelessList)
+
+  searchedGameList = filterList.map((game) => {
+    return (
+    <Game 
+      title={game.title}
+      score={game.score}
+      beaten={game.beaten}
+      comment={game.comment}
+      url={game.url}
+    />)
+  })
+
+  console.log(filterList)
 
   gameList.reverse();
 
   return (
     <div className="App">
       <h1>Sean Murtagh's finished games</h1>
-      {/* <Searchbar onChange={searchTermHandler} value={searchTerm} placeholder="Search here" /> */}
+      <Searchbar onChange={searchTermHandler} value={searchTerm} placeholder="Search here" />
       {!searchFor && gameList}
-      {searchFor && filterList}
+      {searchFor && <div className="searchlist"> {searchedGameList} </div>}
     </div>
   );
 }
