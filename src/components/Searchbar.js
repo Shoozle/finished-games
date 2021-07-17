8f83da1cdd8704fdcde88d01830a5984d3112eca
Shoozle <sean@seanmurtagh.uk>
2021-07-17 13:03:39 +0100

    Better button for cancel search

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,12 +3,14 @@ import classes from './Searchbar.module.css';
 const Searchbar = props => {
     return (
         <div className={classes.Searchbar}>
-            <input
-                value={props.value}
-                onChange={props.onChange}
-                placeholder={props.placeholder}
-            />
-            <button disabled={!props.searched} onClick={props.click}>X</button>
+            <div className={classes.wrapper}>
+                <input
+                    value={props.value}
+                    onChange={props.onChange}
+                    placeholder={props.placeholder}
+                />
+                <button disabled={!props.searched} onClick={props.click}>X</button>
+            </div>
         </div>
     )
 }
