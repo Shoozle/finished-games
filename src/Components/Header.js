b0b0884756f13003f9b26c251f385712a7ddad61
Sean Murtagh <purelolage@googlemail.com>
2023-07-02 20:41:21 +0100

    aborted rebade

--- src/Components/Header.js
+++
@@ -1,6 +0,0 @@
-import classes from './Header.module.css';
-import gamesData from '../data';
-
-const Header = () => <h1 className={classes.header}>Sean Murtaghs {gamesData.length} Finished Games</h1> 
-
-export default Header;
